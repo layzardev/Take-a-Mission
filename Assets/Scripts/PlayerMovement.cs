59f0b355fb70889ccc75a3a44b5df79933a1834c
layzardev <layz4rdev@gmail.com>
2025-09-16 22:41:24 +0700

    Player Move and Jump with Groundcheck
    Player Move and Jump with Groundcheck

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private float Move;
+
+    public Rigidbody2D rb;
+
+    public bool isJumping;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.linearVelocity = new Vector2(speed * Move, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = true;
+        }
+    }
+}
